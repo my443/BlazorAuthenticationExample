75533ace319fd734ad5c7bbfb59370360877fd29
John van Dijk <john@johnv.ca>
2025-11-06 06:56:07 -0500

    Add project files.

---
+++ BlazorAuthenticationExample/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAuthenticationExample
+{
+    public class CustomAuthenticationStateProvider: AuthenticationStateProvider
+    {
+        private ClaimsPrincipal _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(new AuthenticationState(_currentUser));
+        }
+
+        public void Login(string userName)
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+            new Claim(ClaimTypes.Name, userName),
+            new Claim(ClaimTypes.Role, "User")
+        }, "CustomAuth");
+
+            _currentUser = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public void Logout()
+        {
+            _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
---
+++ BlazorAuthenticationExample/Program.cs
@@ -0,0 +1,32 @@
+using BlazorAuthenticationExample;
+using BlazorAuthenticationExample.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
